6e394796a8f4d9986d154c796858958f47749c9c
Will Beasley <wibeasley@hotmail.com>
2016-05-02 21:37:41 -0500

    Link to using LINQ to filter

--- transfer-log/transfer-log/Program.cs
+++ transfer-log/transfer-log/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace transfer_log {
     class Program {
         static void Main( string[] args ) {
+
+            EventLog log_application = new EventLog();
+            log_application.Log = "Application";
+            foreach( EventLogEntry entry in log_application.Entries ) {
+                Console.WriteLine("\tEntry: " + entry.Message);
+            }  
+            //http://stackoverflow.com/questions/5711586/how-to-read-application-log-and-filter-it-based-on-source-in-asp-net
         }
     }
 }
